3514cb65baae8a23437dd2af0d70c8130de7ae86
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-05-14 17:15:57 +0200

    Add event listener to Home component to monitor current window width

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Home() {
+
+    // A state variable specifying the current window width
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    //A function updating the window width
+    const handleWindowSizeChange = () => {
+        setWindowWidth(window.innerWidth);
+    };
+
+    //Add an event listener to monitor the screen width
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowSizeChange);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        }
+
+    });
+
     return(
-        <div>Home</div>
+        <div>{windowWidth}</div>
     );
 }
 

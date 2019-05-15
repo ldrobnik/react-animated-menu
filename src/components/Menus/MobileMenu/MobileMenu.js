cf969cc57aa2284e7c2add9c370f34dcddbf189e
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-05-15 15:21:53 +0200

    Import relevant fontawesome icons into MobileMenu

--- src/components/Menus/MobileMenu/MobileMenu.js
+++ src/components/Menus/MobileMenu/MobileMenu.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {
+    faUserCircle,
+    faPlane,
+    faCreditCard,
+    faLifeRing,
+    faPhone,
+    faSignOutAlt,
+    faQuestionCircle,
+    faInfoCircle} from '@fortawesome/free-solid-svg-icons';
 
 function DesktopMenu(props) {
     return (
         <React.Fragment>
             <div>Mobile Menu</div>
+            <FontAwesomeIcon icon={faInfoCircle} transform={{ rotate: 0 }} />
             <button onClick={props.hideMenu}>Close</button>
         </React.Fragment>
     );

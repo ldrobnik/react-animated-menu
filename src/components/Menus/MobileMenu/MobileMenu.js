35e72cac489daa669a29e0fbd25190a28f4634a1
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-05-15 16:11:21 +0200

    Add array containing link data to MobileMenu

--- src/components/Menus/MobileMenu/MobileMenu.js
+++ src/components/Menus/MobileMenu/MobileMenu.js
@@ -8,13 +8,66 @@ import {
     faPhone,
     faSignOutAlt,
     faQuestionCircle,
-    faInfoCircle} from '@fortawesome/free-solid-svg-icons';
+    faInfoCircle
+} from '@fortawesome/free-solid-svg-icons';
+
+// Menu link data
+const menuLinks = [
+    {
+        name: 'Profile',
+        route: '/profile/',
+        icon: faUserCircle,
+        rotation: 0
+    },
+    {
+        name: 'My Bookings',
+        route: '/bookings/',
+        icon: faPlane,
+        rotation: 315
+    },
+    {
+        name: 'My Payments',
+        route: '/payments/',
+        icon: faCreditCard,
+        rotation: 0
+    },
+    {
+        name: 'Support',
+        route: '/support/',
+        icon: faLifeRing,
+        rotation: 0
+    },
+    {
+        name: 'Contact Us',
+        route: '/contact/',
+        icon: faPhone,
+        rotation: 90
+    },
+    {
+        name: 'Log Out',
+        route: '/logout/',
+        icon: faSignOutAlt,
+        rotation: 0
+    },
+    {
+        name: 'About',
+        route: '/about/',
+        icon: faQuestionCircle,
+        rotation: 0
+    },
+    {
+        name: 'FAQ',
+        route: '/faq/',
+        icon: faInfoCircle,
+        rotation: 0
+    }
+];
 
 function DesktopMenu(props) {
     return (
         <React.Fragment>
             <div>Mobile Menu</div>
-            <FontAwesomeIcon icon={faInfoCircle} transform={{ rotate: 0 }} />
+            <FontAwesomeIcon icon={menuLinks[0].icon} transform={{rotate: menuLinks[0].rotation}}/>
             <button onClick={props.hideMenu}>Close</button>
         </React.Fragment>
     );

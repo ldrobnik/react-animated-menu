b44588053280cd0a864c424010346d1a29bd0f27
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-05-17 18:18:37 +0200

    Create DesktopUserData component

--- src/components/Menus/DesktopMenu/DesktopMainMenu/DesktopMainMenu.js
+++ src/components/Menus/DesktopMenu/DesktopMainMenu/DesktopMainMenu.js
@@ -2,44 +2,9 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 
+import DesktopUserData from './DesktopUserData/DesktopUserData';
+
 /* STYLES */
-
-// User data styles
-
-const UserData = styled.div`
-  width: 100%;
-  text-align: left;
-  font-weight: bold;
-  font-size: 0.9em;
-`;
-
-const UserAvatar = styled.img`
-  
-  height: 2em;
-  width: 2em;
-  
-  margin-right: 1em;
-  padding: 0.5em;
-  
-  border-radius: 50%;
-  border-style: solid;
-  border-width: 2px;
-  border-color: ${props => props.theme.mainColor};
-  box-shadow: 0 0 0 3px ${props => props.theme.lightColor};
-  
-  background-color: ${props => props.theme.lightColor}
-  
-  float: left;
-`;
-
-const UserDetails = styled.div`
-    padding-top: 0.3em;
-    padding-bottom: 1.2em;
-    p {
-    margin: 0;
-    }
-`;
-
 // Styles for container of main menu links
 
 const MainLinks = styled.div`
@@ -96,24 +61,15 @@ function DesktopMainMenu(props) {
         }
     ];
 
-    // Path to the user image
-    const userImagePath = '/' + props.image + '.png';
-
-    // User full name
-    const fullName = props.firstName + ' ' + props.surname;
-
-    // Balance information
-    const balanceInfo = props.balance + ' Available';
-
     return (
         <MainMenu>
-            <UserData>
-                <UserAvatar src={userImagePath} alt='User profile photo'/>
-                <UserDetails>
-                    <p>{fullName}</p>
-                    <p>&pound;{balanceInfo}</p>
-                </UserDetails>
-            </UserData>
+            <DesktopUserData
+                firstName={props.firstName}
+                surname={props.surname}
+                balance={props.balance}
+                image={props.image}
+                hideMenu={props.hideMenu}
+            />
             <MainLinks>
                 {mainLinks.map((link) => (
                     <LinkContainer
---
+++ src/components/Menus/DesktopMenu/DesktopMainMenu/DesktopUserData/DesktopUserData.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+
+/* STYLES */
+const UserData = styled.div`
+  width: 100%;
+  text-align: left;
+  font-weight: bold;
+  font-size: 0.9em;
+`;
+
+const UserAvatar = styled.img`
+  
+  height: 2em;
+  width: 2em;
+  
+  margin-right: 1em;
+  padding: 0.5em;
+  
+  border-radius: 50%;
+  border-style: solid;
+  border-width: 2px;
+  border-color: ${props => props.theme.mainColor};
+  box-shadow: 0 0 0 3px ${props => props.theme.lightColor};
+  
+  background-color: ${props => props.theme.lightColor}
+  
+  float: left;
+`;
+
+const UserDetails = styled.div`
+    padding-top: 0.3em;
+    padding-bottom: 1.2em;
+    p {
+    margin: 0;
+    }
+`;
+
+function DesktopUserData(props) {
+    // Path to the user image
+    const userImagePath = '/' + props.image + '.png';
+
+    // User full name
+    const fullName = props.firstName + ' ' + props.surname;
+
+    // Balance information
+    const balanceInfo = props.balance + ' Available';
+
+    return(
+        <UserData>
+            <UserAvatar src={userImagePath} alt='User profile photo'/>
+            <UserDetails>
+                <p>{fullName}</p>
+                <p>&pound;{balanceInfo}</p>
+            </UserDetails>
+        </UserData>
+    );
+}
+
+export default DesktopUserData;

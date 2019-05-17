5d227c826d7dedb430e626277e4618688dff1e61
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-05-17 18:25:01 +0200

    Create DesktopMainLinks component

---
+++ src/components/Menus/DesktopMenu/DesktopMainMenu/DesktopMainLinks/DesktopMainLinks.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+/* STYLES */
+
+const MainLinks = styled.div`
+  font-weight: bold;
+  clear: left;
+  padding-top: 0.8em;
+  border-top: solid 1px ${props => props.theme.lightMainColor};
+`;
+
+// Link styles
+const LinkContainer = styled.p`
+  margin: 2px;
+  :hover {
+    background-color: ${props => props.theme.lightMainColor};
+  }
+`;
+
+const MenuLink = styled(Link)`
+  color: ${props => props.theme.lightColor};
+  text-decoration: none;
+  padding: 2px;
+`;
+
+function DesktopMainLinks(props) {
+
+    // Links displayed in the main menu
+    const mainLinks = [
+        {
+            name: 'Profile',
+            route: '/profile/' + (props.firstName + props.surname).toLowerCase() + '/'
+        },
+        {
+            name: 'My Bookings',
+            route: '/bookings/'
+        },
+        {
+            name: 'My Payments',
+            route: '/payments/'
+        },
+        {
+            name: 'Log Out',
+            route: '/logout/'
+        },
+        {
+            name: 'Resume Application',
+            route: '/resume/'
+        }
+    ];
+
+
+    return(
+        <MainLinks>
+            {mainLinks.map((link) => (
+                <LinkContainer
+                    key={link.name}
+                    onClick={props.hideMenu}
+                >
+                    <MenuLink to={link.route}>
+                        {link.name}
+                    </MenuLink>
+                </LinkContainer>
+            ))}
+        </MainLinks>
+    );
+}
+
+export default DesktopMainLinks;
--- src/components/Menus/DesktopMenu/DesktopMainMenu/DesktopMainMenu.js
+++ src/components/Menus/DesktopMenu/DesktopMainMenu/DesktopMainMenu.js
@@ -3,33 +3,10 @@ import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 
 import DesktopUserData from './DesktopUserData/DesktopUserData';
+import DesktopMainLinks from './DesktopMainLinks/DesktopMainLinks';
 
 /* STYLES */
-// Styles for container of main menu links
 
-const MainLinks = styled.div`
-  font-weight: bold;
-  clear: left;
-  padding-top: 0.8em;
-  border-top: solid 1px ${props => props.theme.lightMainColor};
-`;
-
-// Link styles
-const LinkContainer = styled.p`
-  margin: 2px;
-  :hover {
-    background-color: ${props => props.theme.lightMainColor};
-  }
-`;
-
-const MenuLink = styled(Link)`
-  color: ${props => props.theme.lightColor};
-  text-decoration: none;
-  padding: 2px;
-`;
-
-
-// Main menu styles
 const MainMenu = styled.div`
   width: 50%;
   display: table-cell;
@@ -37,29 +14,6 @@ const MainMenu = styled.div`
 `;
 
 function DesktopMainMenu(props) {
-    // Links displayed in the main menu
-    const mainLinks = [
-        {
-            name: 'Profile',
-            route: '/profile/' + (props.firstName + props.surname).toLowerCase() + '/'
-        },
-        {
-            name: 'My Bookings',
-            route: '/bookings/'
-        },
-        {
-            name: 'My Payments',
-            route: '/payments/'
-        },
-        {
-            name: 'Log Out',
-            route: '/logout/'
-        },
-        {
-            name: 'Resume Application',
-            route: '/resume/'
-        }
-    ];
 
     return (
         <MainMenu>
@@ -70,18 +24,11 @@ function DesktopMainMenu(props) {
                 image={props.image}
                 hideMenu={props.hideMenu}
             />
-            <MainLinks>
-                {mainLinks.map((link) => (
-                    <LinkContainer
-                        key={link.name}
-                        onClick={props.hideMenu}
-                    >
-                        <MenuLink to={link.route}>
-                            {link.name}
-                        </MenuLink>
-                    </LinkContainer>
-                ))}
-            </MainLinks>
+            <DesktopMainLinks
+                firstName={props.firstName}
+                surname={props.surname}
+                hideMenu={props.hideMenu}
+            />
         </MainMenu>
     );
 
